3e58ca50ac0d2c6951bbe6d375db08b787838ed9
Diego Herrera <diegoricardoherrera@hotmail.com>
2023-04-18 10:29:09 -0700

    Add project files.

---
+++ HolaMundo-Diego/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HolaMundo_Diego.MvcApplication" Language="C#" %>
---
+++ HolaMundo-Diego/Models/HelloWorldModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolaMundo_Diego.Models
+{
+    public class HelloWorldModel
+    {
+        public string Message { get; set; }
+    }
+}
